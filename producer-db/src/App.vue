fd509cb6e595020d9904a04f0dee50475ad85274
Alex Trainham <alextrainham@gmail.com>
2020-04-23 22:12:07 +0000

    noah test from cano server

--- producer-db/src/App.vue
+++ producer-db/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the Producer!!!!!! Alex Test"/>
+    <HelloWorld msg="Welcome to the Producer!!!!!! Alex Test. Now Noah test...."/>
   </div>
 </template>
 
