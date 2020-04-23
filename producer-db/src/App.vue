737c500091651fcb2ecf417744be3de4678bc6eb
Alex Trainham <alextrainham@gmail.com>
2020-04-23 22:08:35 +0000

    hello test

--- producer-db/src/App.vue
+++ producer-db/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the Producer DB"/>
+    <HelloWorld msg="Welcome to the Producer!!!!!! Alex Test"/>
   </div>
 </template>
 
