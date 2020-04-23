2183ad9b64dac71003d6298b1327a2573e756b8f
Noah Weingand <noahweingand@gmail.com>
2020-04-23 22:03:24 +0000

    alex test

--- producer-db/src/App.vue
+++ producer-db/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the Producer Database"/>
+    <HelloWorld msg="Welcome to the Producer DB"/>
   </div>
 </template>
 
