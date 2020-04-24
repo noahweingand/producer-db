3c04114adc6c9e33633f8361130655a07e0815e0
Alex Trainham <alextrainham@gmail.com>
2020-04-24 17:32:45 -0400

    made initial backend server

--- producer-db/server/routes/api/posts.js
+++ producer-db/server/routes/api/posts.js
@@ -16,7 +16,25 @@ router.get('/', async (req, res) => {
 
 })
 
+// add to the db through HTTPS post request
+router.post('/', async (req, res) => {
+    const posts = await makeConnection(); 
+    
+    // TODO : sanitize our queries
 
+    await posts.query('INSERT INTO test_table VALUES(?, ?);', [req.body.num, req.body.string], function(err, res){
+        if(err){
+            console.log('error at posts.js router post function'); 
+            throw err; 
+        }
+        console.log('Post submitted correctly'); 
+    }); 
+
+    res.status(201).send(); 
+}); 
+
+
+// function to make a connection to the database
 async function makeConnection (){
     const con = await mysql.createConnection({
         host: "3.234.246.29", 
