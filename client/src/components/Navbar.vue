f27ace233d6d2e4e3f5208fe00fd9ce6dfc685e6
Alex <alextrainham@gmail.com>
2020-04-29 22:54:19 -0400

    add search dropdown

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -14,22 +14,16 @@
             <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
             <li class="nav-item">
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
             </ul>
             <form class="form-inline my-2 my-lg-0" @submit="letsSee">
+            <b-dropdown split split-variant="outline-success" :text="dropdown" class="mr-2" variant="success">
+                <b-dropdown-item @click="dropListen">All</b-dropdown-item>
+                <b-dropdown-item @click="dropListen">Artists</b-dropdown-item>
+                <b-dropdown-item @click="dropListen">Producers</b-dropdown-item>
+            </b-dropdown>
             <input v-model="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit" value="Submit">Search</button>
             </form>
@@ -44,13 +38,17 @@ export default {
     name: 'Navbar',
     data() {
         return {
-            search: ""
+            search: "", 
+            dropdown: "Search By:"
         }
     },
     methods: {
         letsSee:function(e) {
             alert(this.search) //for testing
             e.preventDefault();
+        }, 
+        dropListen: function(e){
+            this.dropdown = e.srcElement.innerHTML; 
         }
     },
     watch: {
